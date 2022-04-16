81811cf12056324671f20b8c92159b2174149ffb
ArditZubaku <94994431+ArditZubaku@users.noreply.github.com>
2022-04-16 17:13:56 +0200

    Update Shembulli1.py

--- Shembulli1.py
+++ Shembulli1.py
@@ -8,3 +8,12 @@ if selekto=="Enkodim":
     mssgtob64 = base64.b32encode(mssgencoded)
     mssgdecoded = mssgtob64.decode("UTF-8")
     print("Base32 Encoded:", mssgdecoded)
+    
+elif selekto=="Dekodim":
+    mssg=input("Enter your message:")
+    mssgencoded2 = mssg.encode("UTF-8")
+    mssgdecoded2 = base64.b32decode(mssgencoded2)
+    finalmssg = mssgdecoded2.decode("UTF-8")
+    print(finalmssg)
+else:
+    print("Duhet te shkruani Enkodim ose Dekodim")
